fbf652fcf8486bd67bd1608aaf6028673a847326
jourdanmauricio <jourdanmauricio@gmail.com>
2024-02-07 12:51:12 -0300

    .

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,75 +1,52 @@
-console.log(tempData);
+const recommendedContainer = document.getElementById('recommended-container');
+recommendedContainer.innerHTML = '';
 
-const renderReleases = () => {
-  const releasesContainer = document.getElementById('releases-container');
-  releasesContainer.innerHTML = '';
-
-  const htmlReleases = tempData.map((release) => {
-    const figure = document.createElement('figure');
-    const img = document.createElement('img');
-    img.alt = release.title;
-    img.src = release.poster;
-    img.classList.add('releases__image');
-    const figCation = document.createElement('figCation');
-    figCation.classList.add('releases__figcaption');
-    const divInfo = document.createElement('div');
-    divInfo.classList.add('releases__info');
-
-    const title = document.createElement('h3');
-    title.textContent = `${release.title} - ${release.year}`;
-    divInfo.appendChild(title);
-
-    const director = document.createElement('p');
-    const imgDirector = document.createElement('img');
-    imgDirector.src = './../assets/director-chair.svg';
-    director.textContent = release.director;
-    const divDirector = document.createElement('div');
-    divDirector.classList.add('card__icon');
-    divDirector.appendChild(imgDirector);
-    divDirector.appendChild(director);
-    divInfo.appendChild(divDirector);
-
-    const genre = document.createElement('p');
-    const imgGenre = document.createElement('img');
-    imgGenre.src = './../assets/genre.svg';
-    genre.textContent = release.genre;
-    const divGenre = document.createElement('div');
-    divGenre.classList.add('card__icon');
-    divGenre.appendChild(imgGenre);
-    divGenre.appendChild(genre);
-    divInfo.appendChild(divGenre);
-
-    const duration = document.createElement('p');
-    const imgDuration = document.createElement('img');
-    imgDuration.src = './../assets/duration.svg';
-    duration.textContent = release.duration;
-    const divDuration = document.createElement('div');
-    divDuration.classList.add('card__icon');
-    divDuration.appendChild(imgDuration);
-    divDuration.appendChild(duration);
-    divInfo.appendChild(divDuration);
-
-    const rate = document.createElement('p');
-    const imgRate = document.createElement('img');
-    imgRate.src = './../assets/rate.svg';
-    rate.textContent = release.rate;
-    const divRate = document.createElement('div');
-    divRate.classList.add('card__icon');
-    divRate.appendChild(imgRate);
-    divRate.appendChild(rate);
-    divInfo.appendChild(divRate);
-
-    figCation.appendChild(divInfo);
-    figure.appendChild(img);
-    figure.appendChild(figCation);
-
-    const card = document.createElement('article');
-    card.classList.add('releases__card');
-    card.appendChild(figure);
-
-    return card;
+const getMovies = () => {
+  $.get('https://students-api.2.us-1.fl0.io/movies', (data, status) => {
+    if (status === 'success') {
+      renderRecommended(data);
+    } else {
+      recommendedContainer.innerHTML =
+        '<h3>Error obteniendo las películas</h3>';
+    }
+  }).fail(function () {
+    recommendedContainer.innerHTML = '<h3>Error obteniendo las películas</h3>';
   });
-  htmlReleases.forEach((card) => releasesContainer.appendChild(card));
 };
 
-renderReleases();
+const renderRecommended = (movies) => {
+  const htmlRecommended = movies
+    .map(
+      (movie) =>
+        `<article class="recommended__card">
+        <img
+          src="${movie.poster}"
+          alt="${movie.title}">
+        <a href="#">
+          <h3 class="recommended__title">${movie.title} - ${movie.year}</h3>
+        </a>
+        <div class="recommended__info">
+          <div>
+            <img src="/assets/icons/director-chair.svg" alt="">
+            <span>${movie.director}</span>
+          </div>
+          <div>
+            <img src="/assets/icons/genre.svg" alt="">
+            <span>${movie.genre.join(', ')}</span>
+          </div>
+          <div>
+            <img src="/assets/icons/duration.svg" alt="">
+            <span>${movie.duration}</span>
+          </div>
+          <div>
+            <img src="/assets/icons/rate.svg" alt="">
+            <span>${movie.rate}</span>
+          </div>
+        </article>`
+    )
+    .join('');
+
+  recommendedContainer.innerHTML = htmlRecommended;
+};
+
+getMovies();
